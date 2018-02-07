52d9bfe68f797e37eb372067ff31cfd91e26963a
Aleksandra Siabriuk <alexsbrk91@gmail.com>
2018-02-06 23:15:05 -0500

    nycda assigments

---
+++ jschallenges/jschallenges.js
@@ -0,0 +1,45 @@
+
+
+// PART 1 
+
+// Numbers
+
+var alfa = 343;    
+var beta = 752;
+var result = alfa + beta;
+
+console.log(result);   // output to console
+alert(result);   // output as a alert to the web page
+
+console.log( alfa + beta ); // another way to do it
+
+// String
+
+var fname = "Sasha";
+var lname = " Siabriuk";
+var name = fname + lname;
+
+console.log("this person's name is " + name);
+
+
+
+//  PART 2
+
+var interests = [ " climbing", " coding", " design", " traveling"];
+console.log(interests[0] + interests[1]);
+
+console.log(fname + "'s hobbies are " + interests[0] + " and " + interests[1]);
+
+// // PART 3
+
+var number = 10;
+
+if( number < 100) {
+  alert("Hey! Your number is " + number +  " , and it is less than 100!" );
+ } else  {
+   alert("Hey! Your number is " + number + " , and it's greater than 100.");
+ }
+
+// //  PART 4
+
+// //  PART 5
